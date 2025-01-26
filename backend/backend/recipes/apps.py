1d41f6f3c279e0490559528b99668b40a5267eac
Raul2455 <kirillmendrin245@gmail.com>
2025-01-26 15:13:12 +0300

    Эндпоинты исправлены

--- backend/backend/recipes/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecipesConfig(AppConfig):
-    # Указываем имя приложения
-    name = 'recipes'
-
-    # Настраиваем человекочитаемое имя для отображения в админке
-    verbose_name = 'Управление рецептами'
